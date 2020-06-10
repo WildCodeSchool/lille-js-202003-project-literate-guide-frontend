2f9eb7152370dd8142f5a96865b2c65694cb8a47
llaulier <lioraloche@yahoo.fr>
2020-06-10 17:04:23 +0200

    map and leaflet is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Leaflet from './components/map/Map';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello React World</header>
+      <Leaflet />
     </div>
   );
 }
