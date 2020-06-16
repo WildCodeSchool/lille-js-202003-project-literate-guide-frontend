cdac85c0987712648c0b78ad41a229b9daca5fe0
Maxime <maxime.rawinski@gmail.com>
2020-06-16 18:08:51 +0200

    add a drawer when you click on the tag button on the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { IconButton } from '@material-ui/core';
+import { IconButton, SwipeableDrawer, Button } from '@material-ui/core';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Toolbar from '@material-ui/core/Toolbar';
 import TuneIcon from '@material-ui/icons/Tune';
 import NavBarStyles from './NavbarStyles';
 
 export default function NavBar() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(true);
+  };
+
   const classes = NavBarStyles();
   return (
     <div className={classes.root}>
@@ -18,11 +24,58 @@ export default function NavBar() {
             <NotificationsIcon />
           </IconButton>
           <img src="/images/logo.png" alt="logo" className={classes.img} />
-          <IconButton color="inherit">
+          <IconButton onClick={toggleDrawer} color="inherit">
             <TuneIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <div>
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <div className={classes.tags}>
+            <div>
+              <h2 className={classes.title}>
+                Choississez vos centres intérêts
+              </h2>
+            </div>
+            <hr className={classes.hr} />
+            <div>
+              <h2 className={classes.subTitle}>Types</h2>
+            </div>
+            <div>
+              <ul>
+                <li className={classes.subTag}>
+                  <Button className={classes.butt}>Quartier</Button>
+                  <Button className={classes.butt}>Place</Button>
+                  <Button className={classes.butt}>Rue</Button>
+                  <Button className={classes.butt}>Monument</Button>
+                  <Button className={classes.butt}>Musée</Button>
+                  <Button className={classes.butt}>Parc</Button>
+                  <Button className={classes.butt}>Street Art</Button>
+                  <Button className={classes.butt}>Curiosité</Button>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className={classes.subTitle}>Styles</h2>
+            </div>
+            <div>
+              <ul>
+                <li className={classes.subTag}>
+                  <Button className={classes.butt}>Enfants</Button>
+                  <Button className={classes.butt}>Humous</Button>
+                  <Button className={classes.butt}>Quizz</Button>
+                  <Button className={classes.butt}>Teaser</Button>
+                  <Button className={classes.butt}>Name</Button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </SwipeableDrawer>
+      </div>
     </div>
   );
 }
--- src/components/navbar/NavbarStyles.jsx
+++ src/components/navbar/NavbarStyles.jsx
@@ -18,5 +18,38 @@ const NavBarStyles = makeStyles(() => ({
   img: {
     width: '9rem',
   },
+  tags: {
+    background: 'linear-gradient(45deg, #F15348 30%, #DA7B2F 90%)',
+    color: 'white',
+    height: '100%',
+    width: '250px',
+    textAlign: 'center',
+    paddingTop: '30px',
+    borderBottom: '0.1rem solid',
+    borderColor: 'white',
+  },
+  title: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: '1.3rem',
+    paddingBottom: '20px',
+  },
+  subTitle: {
+    fontWeight: 'bold',
+    fontSize: '1.3rem',
+    paddingBottom: '30px',
+    paddingTop: '20px',
+  },
+  hr: {
+    width: '70%',
+    border: '0.1rem solid',
+  },
+  butt: {
+    borderRadius: '60px',
+    borderColor: 'white',
+    border: '0.1rem solid',
+    fontSize: '0.7rem',
+  },
+  subTag: {},
 }));
 export default NavBarStyles;
