4aab2b89d0e2e97aa8075a3d34e902551ec218c6
llaulier <lioraloche@yahoo.fr>
2020-06-29 21:25:05 +0200

    get geolocation

---
+++ src/components/geolocations/useCurrentLocation.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+// getcurrentposition => https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+
+const useCurrentLocation = (options = {}) => {
+  const [location, setLocation] = useState();
+  const [error, setError] = useState();
+  const handleSuccess = (pos) => {
+    const { latitude, longitude } = pos.coords;
+
+    setLocation({
+      latitude,
+      longitude,
+    });
+  };
+  const handleError = (err) => {
+    setError(err.message);
+  };
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+    if (!geolocation) {
+      setError('Geolocation is not supported.');
+      return;
+    }
+    geolocation.getCurrentPosition(handleSuccess, handleError, options);
+  }, [options]);
+
+  return { location, error };
+};
+
+export default useCurrentLocation;
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, { useState } from 'react';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import useCurrentLocation from '../geolocations/useCurrentLocation';
+import useWatchLocation from '../geolocations/useWatchLocation';
+
+const geolocationOptions = {
+  timeout: 1000 * 60 * 3, // 3 min (1000 ms * 60 sec * 3 minute)
+};
 
 const Leaflet = () => {
-  const position = [50.63333, 3.066667];
-
+  useCurrentLocation(geolocationOptions);
+  useWatchLocation(geolocationOptions);
+  useState(true);
+  // console.log(location);
+  // const position = [50.63333, 3.057256];
+  // let position = [{location.latitude}, {location.longitude}]
   return (
-    <Map center={position} zoom={22} style={{ height: '85vh', zIndex: 0 }}>
+    <Map zoom={20} style={{ height: '85vh', zIndex: 0 }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
         attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker className="marker" />
     </Map>
   );
 };
