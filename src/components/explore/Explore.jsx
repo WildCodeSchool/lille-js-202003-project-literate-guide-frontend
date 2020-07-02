d7d5ea74e1b09463164763661643fa49a5206a74
llaulier <lioraloche@yahoo.fr>
2020-07-02 12:11:31 +0200

    context provider are added on geolocation

--- src/components/explore/Explore.jsx
+++ src/components/explore/Explore.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ListIcon from '@material-ui/icons/List';
-import { Fab, Grid } from '@material-ui/core';
+import { Fab } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Leaflet from '../map/Map';
@@ -19,22 +19,16 @@ function Explore() {
 
   return (
     <div>
-      <Grid container>
-        <Grid item xs={12} direction="column">
-          <Leaflet />
-        </Grid>
-        <Grid item xs={12} direction="column">
-          <Fab
-            component={Link}
-            to="/capsules"
-            color="secondary"
-            className={classes.root}
-            aria-label="change"
-          >
-            <ListIcon color="primary" />
-          </Fab>
-        </Grid>
-      </Grid>
+      <Leaflet />
+      <Fab
+        component={Link}
+        to="/capsules"
+        color="secondary"
+        className={classes.root}
+        aria-label="change"
+      >
+        <ListIcon color="primary" />
+      </Fab>
     </div>
   );
 }
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,34 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Map, TileLayer, CircleMarker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
-const geolocationOptions = {
-  enableHighAccuracy: true,
-  timeout: 1000 * 60 * 3, // 3 min (1000 ms * 60 sec * 3 minute)
-  maximumAge: 1000 * 3600 * 24, // 24 hour
-};
+import { LocationContext } from '../../contexts/LocationContext';
 
 const Leaflet = () => {
-  const [position, setPosition] = useState([50.630943, 3.060299]);
-
-  useEffect(() => {
-    const watchID = navigator.geolocation.watchPosition((pos) => {
-      setPosition([pos.coords.latitude, pos.coords.longitude]);
-    });
-    return () => {
-      navigator.geolocation.clearWatch(watchID, geolocationOptions);
-    };
-  }, [setPosition]);
-
+  const value = useContext(LocationContext);
   return (
     <>
-      <Map center={position} zoom={16} style={{ height: '85vh', zIndex: 0 }}>
+      <Map center={value} zoom={16} style={{ height: '85vh', zIndex: 0 }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
           attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <CircleMarker
-          center={position}
+          center={value}
           radius={10}
           color="#FFFFFF"
           fillColor="#F15348"
