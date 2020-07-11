8ffeae195a255498a3a522f48cb4e3fffe01429b
Aurelien <aurelien.badre.dev@gmail.com>
2020-07-11 23:20:59 +0200

    i added the state of parcours and set axios with courses route

--- src/components/parcourslist/ParcoursList.jsx
+++ src/components/parcourslist/ParcoursList.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { backend } from '../../conf';
 // import Leaflet from '../map/Map';
 import './ParcoursList.scss';
 
-function ParcoursList() {
+const ParcoursList = () => {
+  const [parcours, setParcours] = useState([]);
+
+  const getParcours = () => {
+    axios
+      .get(`${backend}/courses`)
+      .then((res) => {
+        setParcours(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getParcours();
+  }, []);
+
   return (
     <div className="parcours">
       <div className="parcoursMap">{/* <Leaflet /> */}</div>
-      <div className="parcoursInfo">
-        <p>test</p>
-      </div>
+      <div className="parcoursInfo">{parcours && <div>test</div>}</div>
     </div>
   );
-}
+};
 
 export default ParcoursList;
