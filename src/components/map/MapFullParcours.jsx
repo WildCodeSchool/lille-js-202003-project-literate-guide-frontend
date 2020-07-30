2852880224a93b2a62e28d8a39c6ff6596a3bbdf
llaulier <lioraloche@yahoo.fr>
2020-07-30 09:33:51 +0200

    fix map

--- src/components/map/MapFullParcours.jsx
+++ src/components/map/MapFullParcours.jsx
@@ -3,42 +3,45 @@ import { Map, TileLayer, CircleMarker } from 'react-leaflet';
 import { LocationContext } from '../../contexts/LocationContext';
 import MarkerFullParcours from '../marker/MarkerFullParcours';
 import 'leaflet/dist/leaflet.css';
+import '../../App.scss';
 
 const MapFullParcours = () => {
   const value = useContext(LocationContext);
 
   return (
     <>
-      <Map
-        id="map"
-        center={value}
-        zoom={16}
-        style={{ height: '100%', zIndex: 0 }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
-          attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <CircleMarker
-          className="circle-back"
+      <div className="content">
+        <Map
+          id="map"
           center={value}
-          radius={30}
-          color="#FFFFFF"
-          fillColor="#F15348"
-          fillOpacity="1"
-          weight="0"
-        />
-        <CircleMarker
-          className="circle-front"
-          center={value}
-          radius={10}
-          color="#FFFFFF"
-          fillColor="#F15348"
-          fillOpacity="1"
-          weight="2"
-        />
-        <MarkerFullParcours />
-      </Map>
+          zoom={16}
+          style={{ height: '100vh', zIndex: 0 }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+            attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <CircleMarker
+            className="circle-back"
+            center={value}
+            radius={30}
+            color="#FFFFFF"
+            fillColor="#F15348"
+            fillOpacity="1"
+            weight="0"
+          />
+          <CircleMarker
+            className="circle-front"
+            center={value}
+            radius={10}
+            color="#FFFFFF"
+            fillColor="#F15348"
+            fillOpacity="1"
+            weight="2"
+          />
+          <MarkerFullParcours />
+        </Map>
+      </div>
     </>
   );
 };
