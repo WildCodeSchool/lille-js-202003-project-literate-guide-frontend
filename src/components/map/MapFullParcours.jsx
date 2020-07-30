15382a282b57294665e397b9667fcd2530c6da83
llaulier <lioraloche@yahoo.fr>
2020-07-30 09:44:39 +0200

    fragment removed

--- src/components/map/MapFullParcours.jsx
+++ src/components/map/MapFullParcours.jsx
@@ -9,40 +9,38 @@ const MapFullParcours = () => {
   const value = useContext(LocationContext);
 
   return (
-    <>
-      <div className="content">
-        <Map
-          id="map"
+    <div className="content">
+      <Map
+        id="map"
+        center={value}
+        zoom={16}
+        style={{ height: '100vh', zIndex: 0 }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+          attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <CircleMarker
+          className="circle-back"
           center={value}
-          zoom={16}
-          style={{ height: '100vh', zIndex: 0 }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
-            attribution='&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <CircleMarker
-            className="circle-back"
-            center={value}
-            radius={30}
-            color="#FFFFFF"
-            fillColor="#F15348"
-            fillOpacity="1"
-            weight="0"
-          />
-          <CircleMarker
-            className="circle-front"
-            center={value}
-            radius={10}
-            color="#FFFFFF"
-            fillColor="#F15348"
-            fillOpacity="1"
-            weight="2"
-          />
-          <MarkerFullParcours />
-        </Map>
-      </div>
-    </>
+          radius={30}
+          color="#FFFFFF"
+          fillColor="#F15348"
+          fillOpacity="1"
+          weight="0"
+        />
+        <CircleMarker
+          className="circle-front"
+          center={value}
+          radius={10}
+          color="#FFFFFF"
+          fillColor="#F15348"
+          fillOpacity="1"
+          weight="2"
+        />
+        <MarkerFullParcours />
+      </Map>
+    </div>
   );
 };
 export default MapFullParcours;
